dc5790dde3a2f548c1c5dc69851f2c3be352c4a1
Manjula Guneratne <uftcodingbootcamp@gmail.com>
2020-12-22 08:05:20 -0500

    upto 12/22

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,22 +1,16 @@
-/*var buttonEl = document.querySelector("#save-task");
-var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-buttonEl.addEventListener("click",function(){
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-});*/
-
-
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
+    event.preventDefault();
+    var taskNameInput  = document.querySelector("input[name='task-name']").value;  
+    console.log(taskNameInput);
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
   };
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
